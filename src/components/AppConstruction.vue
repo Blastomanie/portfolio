d3afe45f12f6be2144376b76581df8f792fb3c55
Blastomanie <my.craft974@gmail.com>
2024-02-13 21:18:43 +0100

    Construction mode

---
+++ src/components/AppConstruction.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img alt=":D" src="../assets/logo.png">
+    <h1>Site en construction !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppConstruction'
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <AppConstruction />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppConstruction from '@/components/AppConstruction.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    AppConstruction
   }
 }
 </script>
